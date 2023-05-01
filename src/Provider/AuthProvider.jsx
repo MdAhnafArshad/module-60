4bc21bf454f21710dea22701944c0423769491cd
MdAhnafArshad <ahnafarshad.me@gmail.com>
2023-05-02 01:47:52 +0600

    added progress bar

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -13,6 +13,7 @@ export const authContext = createContext(null);
 const AuthProvider = ({ children }) => {
 
     const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
 
     //register an account or sign up ................
     const createUser = (email, password) => {
@@ -37,6 +38,7 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             console.log('auth state change', currentUser);
             setUser(currentUser);
+            setLoading(false);
         });
 
         return () => {
@@ -50,6 +52,7 @@ const AuthProvider = ({ children }) => {
         logUser,
         userLogOut,
         user,
+        loading,
     }
 
     return (
--- src/component/route/PrivetRoute.jsx
+++ src/component/route/PrivetRoute.jsx
@@ -6,7 +6,11 @@ import { authContext } from '../../Provider/AuthProvider';
 const PrivetRoute = ({children}) => {
 
 
-    const {user} = useContext(authContext);
+    const {user, loading} = useContext(authContext);
+
+    if(loading){
+        return <progress className="progress progress-error w-56" value="100" max="100"></progress>;
+    }
 
     if(user){
         return children;
